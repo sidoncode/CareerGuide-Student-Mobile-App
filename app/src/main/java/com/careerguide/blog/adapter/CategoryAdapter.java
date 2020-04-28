350c58e352d0f966381092bba21171e467c77744
Osman <osmanjaber786@gmail.com>
2020-04-28 14:50:14 +0530

    Blog UI Changes, Album Adapter CardView

--- app/src/main/java/com/careerguide/blog/adapter/CategoryAdapter.java
+++ app/src/main/java/com/careerguide/blog/adapter/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package com.careerguide.blog.adapter;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,19 +18,26 @@ import com.careerguide.blog.util.Utils;
 
 import java.util.List;
 
+import static com.careerguide.R.layout.lay_category;
+
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
     private Context ctx;
     private List<CatFilter> catFilters;
+    public Typeface fontSemiBold,fontRegular;
+
 
     public CategoryAdapter(Context ctx, List<CatFilter> catFilters) {
         this.ctx = ctx;
         this.catFilters = catFilters;
+        fontSemiBold = Typeface.createFromAsset(ctx.getAssets() , "fonts/Montserrat-SemiBold.ttf");
+        fontRegular = Typeface.createFromAsset(ctx.getAssets() , "fonts/Montserrat-Regular.ttf");
+
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.lay_category, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(lay_category, parent, false);
         return new ViewHolder(v);
     }
 
@@ -39,23 +47,28 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
         holder.id_title.setVisibility(View.GONE);
         holder.id_desc.setVisibility(View.GONE);
         holder.id_desc_pic.setVisibility(View.GONE);
+
+        holder.id_title.setTypeface(fontSemiBold);
+        holder.id_desc.setTypeface(fontRegular);
+
+
         switch (catFilters.get(position).getType()){
             case "title_pic":
                 holder.id_pic.setVisibility(View.VISIBLE);
                 Utils.set_image(ctx, catFilters.get(position).getData(), holder.id_pic);
                 break;
+
             case "title":
-                holder.id_title.setVisibility(View.VISIBLE);
-                holder.id_title.setText(Html.fromHtml(catFilters.get(position).getData()));
-                break;
-            case "paragraph":
-                holder.id_desc.setVisibility(View.VISIBLE);
-                holder.id_desc.setText(Html.fromHtml(catFilters.get(position).getData()));
-                break;
             case "par_heading":
                 holder.id_title.setVisibility(View.VISIBLE);
-                holder.id_title.setText(Html.fromHtml(catFilters.get(position).getData()));
+                holder.id_title.setText(Html.fromHtml(catFilters.get(position).getData()).toString());
                 break;
+
+            case "paragraph":
+                holder.id_desc.setVisibility(View.VISIBLE);
+                holder.id_desc.setText(Html.fromHtml(catFilters.get(position).getData()).toString());
+                break;
+
             case "par_pic":
                 holder.id_desc_pic.setVisibility(View.VISIBLE);
                 Utils.set_image(ctx, catFilters.get(position).getData(), holder.id_desc_pic);
@@ -74,10 +87,12 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
 
         ViewHolder(@NonNull View itemView) {
             super(itemView);
+
             id_pic = itemView.findViewById(R.id.id_pic);
             id_title = itemView.findViewById(R.id.id_title);
             id_desc = itemView.findViewById(R.id.id_desc);
             id_desc_pic = itemView.findViewById(R.id.id_desc_pic);
+
         }
     }
 }
