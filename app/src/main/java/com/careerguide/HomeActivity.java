1cd81dda58a0c147459060d319fea197e0f484c6
Albino Braganza <albinobraganza21@gmail.com>
2020-06-17 10:32:09 +0530

    Improved loading of live stream

--- app/src/main/java/com/careerguide/HomeActivity.java
+++ app/src/main/java/com/careerguide/HomeActivity.java
@@ -143,6 +143,8 @@ public class HomeActivity extends AppCompatActivity implements HomeFragment.OnFr
 
     private String browserKey = Utility.browserKey;
 
+    private boolean dataloaded=false;
+
 
     ArrayList<CurrentLiveCounsellorsModel> finalList=new ArrayList<>();
     ArrayList<CurrentLiveCounsellorsModel> tempCurrentLiveCounsellorsList = new ArrayList<>();
@@ -232,7 +234,7 @@ public class HomeActivity extends AppCompatActivity implements HomeFragment.OnFr
 
         viewModelProvider.setDisplaylistArrayLiveCounsellors(tempCurrentLiveCounsellorsList);
 
-        executeAllTasks();
+
 
         /*
 
@@ -1087,6 +1089,7 @@ public class HomeActivity extends AppCompatActivity implements HomeFragment.OnFr
     protected void onResume() {
         super.onResume();
         new TaskFetchLiveCounsellors().execute();
+
         Utility.handleOnlineStatus(this, "idle");
     }
 
@@ -1445,7 +1448,11 @@ public class HomeActivity extends AppCompatActivity implements HomeFragment.OnFr
                             Log.e("#inside" ,"for" +picUrl+"__"+tempPostLiveCounsellorsList.get(0).getCounsellorName());
 
                         }
-
+                        Log.i("dataloaded",dataloaded+"");
+                        if (!dataloaded){
+                            executeAllTasks();
+                            dataloaded=true;
+                        }
                         if(tempPostLiveCounsellorsList.size()==0){
                             tempPostLiveCounsellorsList.add(new CurrentLiveCounsellorsModel("No one has scheduled anything yet","","","",""));
                         }
